3760d523dbdd5f7d9adc2c071a6879701cedfe8f
Fabrice Bentz <fabrice@weeple.fr>
2019-07-03 14:11:28 +0200

    Restore valid syntax for example.

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -23,7 +23,7 @@ view : Model -> Html msg
 view model =
     let
         parsing =
-            Elastic.parseQuery ")"
+            Elastic.parseQuery "((\"personne me\" (rappelle | recontacte | téléphone) -\"cette personne\" -\"la personne\" -\"une personne\")) | (\"personne ne m'a\" (rappelé | recontacté | téléphoné)) | ((\"m'a\" | \"m'ont\" | \"m'avez\") (jamais | pas) (rappelé | recontacté | téléphoné)) | ((\"devait\" | \"deviez\") ((\"me\" | \"m'\") (\"appeler\" | \"rappeler\" | \"recontacter\" | \"téléphoner\")) | (\"demandé à\" | \"allais\" | \"devais\") \"être\" (\"rappelé\" | \"recontacté\") | \"toujours pas eu d'appel\")"
     in
     case parsing of
         Ok value ->
