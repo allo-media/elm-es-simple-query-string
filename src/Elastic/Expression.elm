64c1819775f80dcf481a7c08b8cb5568804d6f05
Fabrice Bentz <fabrice@weeple.fr>
2019-07-15 13:19:55 +0200

    Improve docs.

--- src/Elastic/Expression.elm
+++ src/Elastic/Expression.elm
@@ -1,9 +1,38 @@
 module Elastic.Expression exposing (Expr(..))
 
-{-| Expr Type define all AST's branchs
+{-| Elastic Expression
+
+Expr can help you to define kind of syntax like And, Or, ....
+
+
+# Definition
+
+@docs Expr
+
 -}
 
 
+{-| An `Expr` Type represent all AST's branchs
+
+    type AST
+        = And AST AST
+        | Or AST AST
+        | Exclude AST
+        | Exact String
+        | Word String
+        | Prefix String
+
+In our case, to follow the production rules `AST` will be `Expr`
+
+    type Expr
+        = And Expr Expr
+        | Exact String
+        | Exclude Expr
+        | Or Expr Expr
+        | Prefix String
+        | Word String
+
+-}
 type Expr
     = And Expr Expr
     | Exact String
