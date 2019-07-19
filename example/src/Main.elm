680c8763ea667a84dfe1cfb35875541b607cf9e7
Fabrice Bentz <fabrice@weeple.fr>
2019-07-19 15:13:15 +0200

    Update example.

--- example/src/Main.elm
+++ example/src/Main.elm
@@ -2,42 +2,53 @@ module Main exposing (main)
 
 import Browser
 import Elastic as Elastic
+import Elastic.Expression exposing (Expr(..))
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Parser exposing (DeadEnd)
 
 
 type alias Model =
-    {}
+    { query : Result (List DeadEnd) Expr }
 
 
 init : Model
 init =
-    {}
+    { query = Ok (Word "") }
 
 
-view : Model -> Html msg
+type Msg
+    = Parse (Result (List DeadEnd) Expr)
+
+
+view : Model -> Html Msg
 view model =
-    let
-        parsing =
-            Elastic.parseQuery "big* (potatoes|\"french fries\") -salad"
-    in
-    case parsing of
-        Ok value ->
-            div []
-                [ Elastic.serializeExpr value |> text
-                , hr [] []
-                , Debug.toString value |> text
-                ]
+    div []
+        [ input [ onInput (Parse << Elastic.parseQuery) ] []
+        , div []
+            [ case model.query of
+                Ok value ->
+                    div []
+                        [ Elastic.serializeExpr value |> text
+                        , hr [] []
+                        , Debug.toString value |> text
+                        ]
 
-        Err err ->
-            Debug.toString err |> text
-
-
-update : msg -> Model -> Model
-update _ model =
-    model
+                Err err ->
+                    Debug.toString err |> text
+            ]
+        ]
 
 
-main : Program () Model msg
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Parse result ->
+            { model | query = result }
+
+
+main : Program () Model Msg
 main =
     Browser.sandbox
         { init = init
