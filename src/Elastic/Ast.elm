98841dfd4209a43257c79b3fd0631d6c6623bbf9
Nicolas Perriault <nicolas@perriault.net>
2019-08-09 15:32:54 +0200

    Refactor join operator handling.

--- src/Elastic/Ast.elm
+++ src/Elastic/Ast.elm
@@ -12,43 +12,47 @@ type Ast
     | Word String
 
 
-joinAnd : Ast -> Ast -> List Expr -> List Expr
-joinAnd first second acc =
-    case ( first, second ) of
-        ( And a b, And c d ) ->
-            List.concat [ acc, joinAnd a b acc, joinAnd c d acc ]
+isAnd : Ast -> Maybe ( Ast, Ast )
+isAnd ast =
+    case ast of
+        And a b ->
+            Just ( a, b )
 
-        ( And a b, y ) ->
-            List.concat [ acc, joinAnd a b acc, [ toExpr y ] ]
-
-        ( x, And c d ) ->
-            List.concat [ acc, [ toExpr x ], joinAnd c d acc ]
-
-        ( x, y ) ->
-            [ toExpr x, toExpr y ]
+        _ ->
+            Nothing
 
 
-joinOr : Ast -> Ast -> List Expr -> List Expr
-joinOr first second acc =
-    case ( first, second ) of
-        ( Or a b, Or c d ) ->
-            List.concat [ acc, joinOr a b acc, joinOr c d acc ]
+isOr : Ast -> Maybe ( Ast, Ast )
+isOr ast =
+    case ast of
+        Or a b ->
+            Just ( a, b )
 
-        ( Or a b, y ) ->
-            List.concat [ acc, joinOr a b acc, [ toExpr y ] ]
+        _ ->
+            Nothing
 
-        ( x, Or c d ) ->
-            List.concat [ acc, [ toExpr x ], joinOr c d acc ]
 
-        ( x, y ) ->
-            [ toExpr x, toExpr y ]
+join : (Ast -> Maybe ( Ast, Ast )) -> Ast -> Ast -> List Expr -> List Expr
+join isOperator first second acc =
+    case ( isOperator first, isOperator second ) of
+        ( Just ( a, b ), Just ( c, d ) ) ->
+            List.concat [ acc, join isOperator a b acc, join isOperator c d acc ]
+
+        ( Just ( a, b ), Nothing ) ->
+            List.concat [ acc, join isOperator a b acc, [ toExpr second ] ]
+
+        ( Nothing, Just ( c, d ) ) ->
+            List.concat [ acc, [ toExpr first ], join isOperator c d acc ]
+
+        ( Nothing, Nothing ) ->
+            [ toExpr first, toExpr second ]
 
 
 toExpr : Ast -> Expr
 toExpr expr =
     case expr of
         And first second ->
-            Expr.And (joinAnd first second [])
+            Expr.And (join isAnd first second [])
 
         Exact string ->
             Expr.Exact string
@@ -57,7 +61,7 @@ toExpr expr =
             Expr.Exclude (toExpr first)
 
         Or first second ->
-            Expr.Or (joinOr first second [])
+            Expr.Or (join isOr first second [])
 
         Prefix string ->
             Expr.Prefix string
